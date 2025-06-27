c16dc12c047a6b892f9da1deb421e78250b95aa9
Hernan Rocca <69273873+RockBanzai@users.noreply.github.com>
2025-06-27 21:27:51 +0200

    Allow configuring port via settings

--- BcodeSeed.Api/Program.cs
+++ BcodeSeed.Api/Program.cs
@@ -1,5 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var urls = builder.Configuration["Urls"] ?? builder.Configuration["URLS"];
+if (!string.IsNullOrWhiteSpace(urls))
+{
+    builder.WebHost.UseUrls(urls);
+}
+else if (builder.Configuration.GetValue<int?>("PORT") is { } port)
+{
+    builder.WebHost.UseUrls($"http://*:{port}");
+}
+
 // Add services to the container.
 
 builder.Services.AddControllers();
